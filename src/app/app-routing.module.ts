1a2214673d87b717aa2f65ef39c53492419047df
Patricio Sepulveda <pato.sepulveda@hotmail.es>
2021-04-26 01:17:02 -0400

    termino de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogComponent } from './log/log.component';
+import { ContentComponent } from './view/content/content.component';
+import { DefaultMenuComponent } from './view/content/default-menu/default-menu.component';
+import { Folder1Component } from './view/content/folder1/folder1.component';
+import { Folder2Component } from './view/content/folder2/folder2.component';
+import { NavComponent } from './view/content/nav/nav.component';
+import { MenuComponent } from './view/menu/menu.component';
+import { ViewComponent } from './view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LogComponent},
+  {path: 'main', component:  ViewComponent, children: [
+    {path: 'mainPage', component: DefaultMenuComponent},
+    {path: 'folders', component: Folder1Component},
+    {path: 'folders/forms', component: Folder2Component}
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
